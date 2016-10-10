b52cb93f8bdf49bec2fe2b2f6da011082cc00a3c
Lucas Courot <lucas@courot.com>
2016-10-11 01:15:31 +0200

    Configure phpspec

---
+++ tests/spec/MoodValue/Model/User/UserSpec.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace spec\MoodValue\Model\User;
+
+use MoodValue\Model\User\User;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class UserSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(User::class);
+    }
+}
